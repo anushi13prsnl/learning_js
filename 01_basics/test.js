32b434802a6ee514e5f23101b56e4735ddcddd7d
Anushi Rajput <145750945+anushi13prsnl@users.noreply.github.com>
2023-11-10 20:52:51 +0000

    commit&saving_procedure_explained

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,21 @@
 console.log("hi")
+
+/* HOW TO SET UP ENVIRONMENT TO CODE JS IN GITHUB!
+   
+   created an account on github
+   made new repo named learning_js
+   click on code=> create codespace on main => on cmd palette type >container..add dev container config files
+   select node js & js
+   select default version
+   ok
+   Select Rebuild now (if prompt comes)
+   create your new folder n files n start coding
+
+   TO SAVE N COMMIT!
+   go to 4th option source control on left
+   there select files to commit & write a msg (takes your files to staging area)
+   click on 3-dots option => push, that's all!
+
+   CAN VIEW AFTER RE-LOADING your commits and saved material!
+
+ */
