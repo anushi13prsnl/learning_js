c5751b403e244b6c126df626401cae506b62bcc8
Anushi Rajput <145750945+anushi13prsnl@users.noreply.github.com>
2023-11-11 09:36:46 +0000

    learnt about datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //to treat all js code in newer version | & once written sara code new version k hisaab se hi treat hoga no looking back
+
+//alert("hi")      //runs on browser not on nodejs
+// ; are required to start new line in the same line like below code but if already went to next line no need
+
+//console.log("hi"); console.log("world!")
+// REMEMBER CODE SHOULD BE WRITTEN NEATLY!
+
+//************************************data_types*********************************************/
+//number (includes int float wghra sbh)
+//bigint (for very big values)
+//string => ""
+//boolean => true/false
+//null => standalone value(representation of empty value) bbhi + datatype bhi
+//undefined => variable declared but not initialized with any value
+//symbol => to find uniqueness, (will study when will do reactjs)
+
+//objects(study later, wait!===>>>>)
+
+//typeof 2 wyas => passing value | passing variable
+let name = "ramu";
+console.log(typeof "ramu"); 
+console.log(typeof name);
+
+console.log(typeof null);     //prints object | yh lang. ki problem hi k null datatype bhi h + object nhi (will see it later>>)
+console.log(typeof undefined);//prints undefined | mtlb undefined ka type undefined hi h
